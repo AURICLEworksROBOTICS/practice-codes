4a2139c693ddc78fd22d9512682b05c8d98afd31
AURICLE <70269704+AURICLEworksROBOTICS@users.noreply.github.com>
2020-12-08 16:41:12 +0530

    Add files via upload

---
+++ convert celsius to fehrenheit !.py
@@ -0,0 +1,15 @@
+
+#     visit our website : https://sites.google.com/view/auricle-robotics/home
+
+#!/usr/bin/env python
+Celsius = int(raw_input("Enter a temperature in Celsius: "))
+  #special credits to : http://pythonfiddle.com/temperature-convert-program/
+Fahrenheit = 9.0/5.0 * Celsius + 32
+   
+
+    # visit our website : https://sites.google.com/view/auricle-robotics/home
+
+print "Temperature:", Celsius, "Celsius = ", Fahrenheit, " F"
+      
+    #    visit our website : https://sites.google.com/view/auricle-robotics/home
+    
